0819a4dccb8ff90a398f6ec8c6ab7b7b8903b680
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-05-17 04:00:25 +1000

    rm old comment

--- flake.nix
+++ flake.nix
@@ -13,25 +13,22 @@
     overlays.default = final: prev: {
       cheat = self.packages.${prev.system}.default;
     };
-    packages = utils.lib.eachSystem { } (pkgs:
-      # TODO: Linux patch
-      # TODO: Linux build
-      {
-        default = pkgs.stdenv.mkDerivation {
-          name = "cheat";
-          version = "2.0.2";
-          src = ./.;
-          nativeBuildInputs = with pkgs; [ autoconf gnumake nasm patch ];
-          configurePhase = "autoconf -i && ./configure";
-          buildPhase = "make build";
-          installPhase = "install -D cheat $out/bin/cheat";
-          meta = with pkgs.lib; {
-            description = "TUI cheat utility, optimized for forgetfulness";
-            homepage = "https://github.com/cheat/cheat";
-            license = licenses.mit;
-            platforms = platforms.all;
-          };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        name = "cheat";
+        version = "2.0.2";
+        src = ./.;
+        nativeBuildInputs = with pkgs; [ autoconf gnumake nasm patch ];
+        configurePhase = "autoconf -i && ./configure";
+        buildPhase = "make build";
+        installPhase = "install -D cheat $out/bin/cheat";
+        meta = with pkgs.lib; {
+          description = "TUI cheat utility, optimized for forgetfulness";
+          homepage = "https://github.com/cheat/cheat";
+          license = licenses.mit;
+          platforms = platforms.all;
         };
-      });
+      };
+    });
   };
 }
